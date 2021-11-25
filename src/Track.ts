afd0f7f0fbe2bfc6f192dfc416f377c4308dfa49
Conrad Scherb <conrad.scherb@heartlab.ai>
2021-11-26 01:10:44 +1300

    Add album art fetch & support for "ID - ID"

--- src/Track.ts
+++ src/Track.ts
@@ -3,6 +3,7 @@ export interface Track {
     fullTrackName: string;
     artist: string;
     trackName: string;
+    albumArt: string | undefined;
     url: string | undefined;
     nextURL: string | undefined;
     prevURL: string | undefined;
--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,17 @@ export async function getTracklist(url: string): Promise<Track[]> {
     const response = await AxiosInstance.get(baseURL + "tracklist/" + url);
     const html = response.data;
     const pageHTML = cheerio.load(html);
-    const tracksTable = pageHTML('*[itemprop = "tracks"]');
+    const tracksTable = pageHTML('.tlpTog');
 
     const tracksList: Track[] = [];
 
-
     tracksTable.each((i, el) => {
-        const fullTrackName = pageHTML(el).find('*[itemprop = "name"]').attr('content');
-        const url = pageHTML(el).find('*[itemprop = "url"]').attr('content');
+        const artworkURL = pageHTML(el).find(".artwork").attr("data-src");       
+        const trackHTML = pageHTML(el).find("*[itemprop = 'tracks']");
+
+        const fullTrackName = trackHTML.find('*[itemprop = "name"]').attr('content') ?? "ID - ID";
+        const url = trackHTML.find('*[itemprop = "url"]').attr('content');
+
         const artist = fullTrackName.substr(0, fullTrackName.lastIndexOf("-")-1);
         const trackName = fullTrackName.substr(fullTrackName.lastIndexOf("-")+2);
 
@@ -33,15 +36,16 @@ export async function getTracklist(url: string): Promise<Track[]> {
             fullTrackName: fullTrackName,
             artist: artist,
             trackName: trackName,
+            albumArt: artworkURL,
             url: url,
             prevURL: prevURL,
             nextURL: nextURL
         }
-
+        
         tracksList.push(newTrack);
     })
 
-    return tracksList;;
+    return tracksList;
 }
 
 getTracklist("h4gcxq1/k-motionz-ukf-on-air-hyper-vision-2020-07-03.html").then((tracks) => {
